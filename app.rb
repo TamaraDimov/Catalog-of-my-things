85becf330c0a8e2efe34cb8b43b38c9593ad8889
TamaraDimov <tamemojsoska@gmail.com>
2023-08-07 12:44:15 +0200

    finish group tasks

---
+++ app.rb
@@ -0,0 +1,19 @@
+class App
+  def list_all_books; end
+
+  def list_all_music_albums; end
+
+  def list_of_games; end
+
+  def list_all_labels; end
+
+  def list_all_authors; end
+
+  def list_all_sources; end
+
+  def add_a_book; end
+
+  def add_a_music_album; end
+
+  def add_a_game; end
+end
